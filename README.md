bb05e409e452eeac5fed910f29883b6b8325c820
tahbjee <noljko13@gmail.com>
2023-12-30 10:20:00 -0300

    first commit

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 "# RockPaperScissors" 
 "# RockPaperScissors" 
 # RockPaperScissors
+"# RockPaperScissors" 
--- app.js
+++ app.js
@@ -1,92 +1,46 @@
 
 
-
-
-// function  getComputerChoice(array) {
-//     const randomIndex = Math.floor(Math.random() * array.length);
-//     return array[randomIndex];
-//   }
-  
-
-//   const choice1 = ['Rock', 'Paper', 'Scissors'];
-// //   console.log(getComputerChoice(choice1)); // Outputs a random fruit
-  
-
-
-// function playRound(playerSelection, computerSelection) {
-//     // Compare with consistent casing
-//     if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
-//       console.log('You lose, paper beats rock!');
-//     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
-//         console.log('You win, paper beats rock!');
-//     }  else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
-//         console.log('You lose, scissors beats rock!');
-//     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
-//         console.log('You win, rock beats scissors!');
-
-//     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
-//         console.log('You lose, rock beats scissors!');
-
-//     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
-//         console.log('You win, scissors beats paper!');
-//     } 
-//     // tie
-//     else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'paper') {
-//         console.log('It\'s a tie!');
-//         playRound();
-//         return;
-//     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock') {
-//     console.log('It\'s a tie!');
-//     playRound();
-//     return;
-//     }else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'scissors') {
-//     console.log('It\'s a tie!');
-//     playRound();
-//     return;
-//     }else{
-//     console.log('Enter a suportted value. Rock, paper or scissors.')
-//   }};
-   
-//   const playerSelection = prompt('Choose one between (rock, paper, scissors): ');
-//   const computerSelection = getComputerChoice(choice1);
-//   console.log(playRound(playerSelection, computerSelection));
-
 function getComputerChoice(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
-}
+    }
+    
+    
+    const array = ['Rock', 'Paper', 'Scissors'];
 
-function playRound(playerSelection, computerSelection) {
-    // Convert both selections to lowercase for case-insensitive comparison
-    const player = playerSelection.toLowerCase();
-    const computer = computerSelection.toLowerCase();
-
-    // Compare selections
+ function playRound(playerSelection, computerSelection) {
+    // your code here!
     if (player === 'rock' && computer === 'paper') {
-        console.log('You lose, paper beats rock!');
+        return 'You lose, paper beats rock!';
     } else if (player === 'paper' && computer === 'rock') {
-        console.log('You win, paper beats rock!');
+        return 'You win, paper beats rock!';
     } else if (player === 'paper' && computer === 'scissors') {
-        console.log('You lose, scissors beats paper!');
+        return 'You lose, scissors beats paper!';
     } else if (player === 'rock' && computer === 'scissors') {
-        console.log('You win, rock beats scissors!');
-    } else if (player === 'scissors' && computer === 'rock') {
-        console.log('You lose, rock beats scissors!');
-    } else if (player === 'scissors' && computer === 'paper') {
-        console.log('You win, scissors beats paper!');
-    } else if (player === computer) {
-        // Tie
-        console.log('It\'s a tie!');
 
+        return 'You win, rock beats scissors!';
+    } else if (player === 'scissors' && computer === 'rock') {
+
+        return 'You lose, rock beats scissors!';
+    } else if (player === 'scissors' && computer === 'paper') {
+        return 'You win, scissors beats paper!';
+    } else if (player === computer) {
         const newPlayerSelection = prompt('Choose again between (rock, paper, scissors): ');
         const newComputerSelection = getComputerChoice(choice1);
         return playRound(newPlayerSelection, newComputerSelection);
     } else {
-        console.log('Enter a supported value. Rock, paper, or scissors.');
+        return 'Enter a supported value. Rock, paper, or scissors.';
     }
 }
 
-const choice1 = ['rock', 'paper', 'scissors'];
-const playerSelection = prompt('Choose one between (rock, paper, scissors): ');
-const computerSelection = getComputerChoice(choice1);
+
+const newPlayerSelection = prompt('Choose again between (rock, paper, scissors): ').toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
 console.log(playRound(playerSelection, computerSelection));
+
+
+    
+
+   
+         
+         
